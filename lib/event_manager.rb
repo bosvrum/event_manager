eef9948913f222f8a37d44edca94f8bcb25663fc
Catalin <catalinboruga@yahoo.com>
2015-07-08 18:46:58 +0200

    Display the zip code al all attendees

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,18 @@
+require "csv"
 puts "EventManager Initialized!"
 
-# contents = File.read("event_attendees.csv")
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-	next if index == 0
-	columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+	name = row[:first_name]
+	zipcode = row[:zipcode]
+	if zipcode.nil?
+		zipcode = "00000"
+	elsif	zipcode.length < 5
+		zipcode = zipcode.rjust(5, "0")
+	elsif zipcode.length > 5
+		zipcode = zipcode[0..4]
+	end
+
+	puts "#{name} - #{zipcode}"
 end
