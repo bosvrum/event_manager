9fc8815dc11c90807a13a80788cd4b1b53b4d15d
Catalin <catalinboruga@yahoo.com>
2015-07-08 20:00:33 +0200

    Add api key

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,21 @@
-require "csv"
-puts "EventManager Initialized!"
+require 'sunlight-congress'
+require 'rubygems'
+require 'csv'
+
+
+
+Sunlight::Congress.api_key = "28ec1945859a4502a1477f6e42d8e36e"
 
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+puts "EventManager initialized."
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
 	name = row[:first_name]
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 	zipcode = clean_zipcode(row[:zipcode])
-	puts "#{name} - #{zipcode}"
+	puts "#{name} - #{zipcode} - #{legislators}"
 end
